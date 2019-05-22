59a266dd3dd5259f8f79234ae92646aed86a7d1f
Admin <Admin@DESKTOP-POQE336>
2019-05-22 14:16:33 +1200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace c_sharp6
+{
+    //program to convert from Celsius degrees to Kelvin and Fahrenheit
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter the celsius:");
+            int celsius = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("kelvin={0}", celsius + 273);
+            Console.WriteLine("fahrenheit={0}", celsius * 18 / 10 + 32);
+
+        }
+    }
+}
